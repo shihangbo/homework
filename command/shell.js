3d976194ebd19a193ee58b294c5c2c26cf2d5841
shihangbo <shihangbo@126.com>
2018-03-23 17:52:45 +0800

    update_add: vi/vim

---
+++ command/shell.js
@@ -0,0 +1,6 @@
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/
+ * @content 
+ */
---
+++ command/vi&vim.js
@@ -0,0 +1,22 @@
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/03/20
+ * @content vi vim
+
+		vim xxx.js 在终端用vim编辑器新建一份xxx.js文件（未保存，没有写入磁盘的文件）
+		:(shift + ;) 命令模式
+		esc键 返回一般模式
+		i键 进入编辑模式
+		o键 进入编辑模式，且光标定位到下一行
+		w 储存
+ 		q 退出
+ 		cat xxx.js 在终端查询文件内容
+		命令模式 sh 把vim放入后台执行
+		命令模式 exit 返回之前vim界面
+		命令模式 :wq! w保存 q退出 !保存强制退出
+		命令模式 :w xxx.js 另存为文件名xxx.js的文件
+
+ */
+
+
