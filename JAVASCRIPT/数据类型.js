6bb3307a7861f460f1d305dfa4317f2d0eb795b6
shihangbo <shihangbo@126.com>
2018-03-25 10:16:25 +0800

    update_add: 数据类型

---
+++ JAVASCRIPT/数据类型.js
@@ -0,0 +1,22 @@
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/03/21
+ * @content javascript数据类型
+
+	null
+	nudefined
+	boolean
+	string
+	number
+	object
+
+	object属性和方法
+		1.toString(): 返回对象的字符串表示
+		2.valueOf():  返回对象的字符串、数值或布尔值表示；
+
+
+
+
+ */
+
