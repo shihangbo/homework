5c5206b5d25a16fbb98aed24b0d1024523dd5943
shihangbo <shihangbo@126.com>
2018-03-28 23:31:57 +0800

    fixed_fix: fix；

--- react-build/src/index.js
+++ react-build/src/index.js
@@ -9,60 +9,49 @@
 
 const React = {
 	// creactElement方法返回的对象记录这个dom节点的所有信息，换言之，通过它就可以生成真正的dom，这个记录信息的对象就是虚拟dom
-	createElement: function(tag, attrs, ...children) {
-		//组件化之路 如果tag是一个方法，那么它是一个组件
-		if (typeof tag === 'function') {
-			return tag(attrs || {})
-		}
+	createElement
+}
 
-		return {
-			tag,
-			attrs,
-			children
-		}
+function createElement(tag, attrs, ...children) {
+	return {
+		tag, attrs, children
 	}
 }
 
 const ReactDOM = {
-	//将虚拟dom渲染成真实dom
-	vnodeToRealDom: function(vnode, container) {
-		//当vnode为字符串时，渲染结果是一段文本
-		if (typeof vnode === 'string' || typeof vnode === 'number') {
-			const textNode = document.createTextNode(vnode)
-			return container.appendChild(textNode)
-		}
-
-		//获取vnonde第一层元素标签
-		const dom = document.createElement(vnode.tag)
-		//给获取的元素添加属性
-		if (vnode.attrs) {
-			Object.keys(vnode.attrs).forEach(key => {
-
-				const value = vnode.attrs[key]
-
-				if (key === 'className') key = 'class'
-
-				//如果是事件监听函数，则直接附加到dom上
-				if (typeof value === 'function') {
-					dom[key.toLowerCase()] = value
-				} else {
-					dom.setAttribute(key, value)
-				}
-
-			})
-		}
-		//遍历当前虚拟dom的子元素，递归生成各子元素
-		vnode.children && vnode.children.forEach(child => ReactDOM.vnodeToRealDom(child, dom))
-
-		//返回元素片段，即将渲染结果挂载到真正的dom上
-		return container.appendChild(dom)
-	},
-	//实现vnodeToRealDom方法
 	render: (vnode, container) => {
-		//多次调用render函数，清楚原来内容
 		container.innerHTML = ''
-		return ReactDOM.vnodeToRealDom(vnode, container)
+		return render(vnode, container)
 	}
+}
+
+function render(vnode, container) {
+	if(typeof vnode === 'string' || typeof vnode === 'number') {
+		const textNode = document.createTextNode(vnode)
+		return container.appendChild(textNode)
+	}
+
+	const dom = document.createElement(vnode.tag)
+
+	if(vnode.attrs) {
+		Object.keys(vnode.attrs).forEach(key => {
+			const value = vnode.attrs[key]
+
+			if(key === 'className') key = 'class'
+
+			if(typeof value === 'function') {
+				dom[key.toLowerCase()] = value
+			} else {
+				dom.setAttribute(key, value)
+			}
+
+		})
+	}
+
+	vnode.children.forEach(child => render(child, dom))
+
+	return container.appendChild(dom)
+
 }
 
 //组件化之路 函数组件Welcome
@@ -82,17 +71,15 @@ function App () {
 const element = <Welcome name='WATSON' />
 const app = <App />
 
-//组件化之路 类组件 
 
-// ReactDOM.render(
-// 	<h1>hello <span>watson!</span></h1>,
-// 	document.getElementById('root')
-// )
+function onClick() {
+	console.log('方法在哪里注册的？')
+}
 
 function tick() {
 	const element = (
 		<div>
-			<h1>hello watson!</h1>
+			<h1 onClick={() => onClick()}>hello watson!</h1>
 			<h2>it is {new Date().toLocaleTimeString()}</h2>
 		</div>
 	)
