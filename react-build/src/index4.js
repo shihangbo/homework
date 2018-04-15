40be828872bd6cfd6883bd894f207e555bb9a050
shihangbo <shihangbo@126.com>
2018-04-15 22:47:55 +0800

    fixed_fix: 对于react组件化的深入理解；

--- react-build/src/index4.js
+++ react-build/src/index4.js
@@ -38,7 +38,7 @@ function _render(vnode, container) {
 	}
 
 	if (typeof vnode.tag === 'function') {
-		// createComponent方法用来创建组件实例，并且将函数定义组件拓展为类定义组件进行处理，以免其他地方需要区分不同定义方式
+		// createComponent方法用来创建组件实例，将函数定义组件拓展为类定义组件进行处理，以免其他地方需要区分不同定义方式
 		const component = createComponent(vnode.tag, vnode.attrs)
 		// setComponentProps方法用来更新props，在其中可以实现componentWillMount，componentWillReceiveProps两个生命周期方法
 		setComponentProps(component, vnode.attrs)
@@ -74,11 +74,45 @@ function createComponent(component, props) {
 			return this.constructor(props)
 		}
 	}
-
+	console.log(inst)
 	return inst
 }
 
+/**
+ * 类组件：inst数据结构
+ * Counter {
+ * 		base: { div },     // base对象是当前node节点信息，包含属性，子节点，事件等
+ * 		props: null,       // 从父节点传递的参数
+ * 		state: { num: 1 }  // 组件私有属性
+ * }
+ * 
+ * 函数组件：inst数据结构
+ * Watson {
+ * 		base: { div },     // base对象是当前node节点信息，包含属性，子节点，事件等
+ * 		props: null,       // 从父节点传递的参数
+ * 		state: {}          // 组件私有属性
+ * 		constructor: function Watson(){}
+ * 		render: function (){}  // 我猜是 React.cteateElememt()函数
+ * }
+**/
+
+/**
+ * setComponentProps函数解读
+
+	if (如果component实例没有base属性) {
+		执行component实例定义的 componentWillMount() 函数
+	} else if (如果component实例存在 componentWillReceiveProps函数) {
+		执行component实例定义的 componentWillReceiveProps() 函数
+	}
+
+	给实例对象添加 props 属性
+
+	调用renderComponent(实例对象) 方法
+ */
+
+
 function setComponentProps(component, props) {
+	console.log(component.componentWillReceiveProps)
 	if (!component.base) {
 		if (component.componentWillMount) component.componentWillMount()
 	} else if (component.componentWillReceiveProps) {
@@ -88,7 +122,25 @@ function setComponentProps(component, props) {
 	renderComponent(component)
 }
 
+/**
+ * renderComponent函数解读
+
+	调用实例的render函数，得到实例的虚拟dom对象
+
+	if(实例是否存在base对象 && 实例是否存在componentWillUpdate函数) {
+		执行 componentWillUpdate() 函数
+	}
+
+	【重点】: base对象 记录dom元素信息
+	base = 执行 _render(renderer)
+
+
+
+
+ */
+
 function renderComponent(component) {
+
 	let base
 
 	const renderer = component.render()
@@ -163,12 +215,27 @@ class Counter extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			num: 1
+			num: props.num
 		}
 	}
 	onclick() {
-		console.log(this)
-		this.setState({num: this.state.num + 1})
+		this.setState({num: watson += 1})
+	}
+	componentWillMount() {
+		console.log('componentWillMount')
+	}
+	componentDidMount() {
+		console.log('componentDidMount')
+	}
+
+	componentWillReceiveProps() {
+		console.log('componentWillReceiveProps')
+	}
+	componentWillUpdate() {
+		console.log('componentWillUpdate')
+	}
+	componentDidUpdate() {
+		console.log('componentDidUpdate')
 	}
 	render() {
 		return (
@@ -183,12 +250,20 @@ class Counter extends Component {
 	
 }
 
-// 方法绑定的两种方式
+// 方法绑定的三种方式
 // 1. onClick={onClick} runtime:函数onClick执行时this对象绑定到当前元素，推荐！
 // 2. onClick={() => onClick()} runtime:函数onClick执行时this对象指向window，不推荐！
+// 3. onClick={() => this.onClick()} runtime: ?
 
+function Watson(props) {
+	return (
+		<h1 data-url="nnnnnn"><span className="aaa">Hi, </span>{this.props.name}</h1>
+	)
+}
+
+var watson = 1
 ReactDOM.render(
-	<Counter />,
+	<Counter num={watson} />,
 	document.getElementById('root')
 )
 
