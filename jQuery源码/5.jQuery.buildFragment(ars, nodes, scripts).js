d094b09a1741d2d28035ed531712e484ac4dc3b3
shihangbo <shihangbo@126.com>
2018-05-23 19:36:23 +0800

    Add

---
+++ jQuery源码/5.jQuery.buildFragment(ars, nodes, scripts).js
@@ -0,0 +1,9 @@
+
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/05/20
+ * @content
+ */
+
+var content = '方法 jQuery.buildFragment() 先创建一个文档片段 DocumentFragment，然后调用 jQuery.clean()方法将 HTML代码转换为 DOM元素，并存储在创建的文档片段中'
