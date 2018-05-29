8cbcc69b0af50aee2aec668e2a896d10e4bba9be
shihangbo <shihangbo@126.com>
2018-05-29 16:54:12 +0800

    add

---
+++ jQuery源码/6.jQuery.clean(elem, context, fragment, scripts).js
@@ -0,0 +1,9 @@
+
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/05/20
+ * @content
+ */
+
+var content = '方法 jQuery.clean(elems, context, fragment, scripts) 负责把 HTML代码转换成 DOM元素，并提取其中的 script元素'
