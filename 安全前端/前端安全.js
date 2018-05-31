a73a06818abe8d59298d141568c11edd8bcdac87
shihangbo <shihangbo@126.com>
2018-05-31 22:54:15 +0800

    add

---
+++ 安全前端/前端安全.js
@@ -0,0 +1,14 @@
+/**
+ * @author watson
+ * @version V1.0
+ * @since 2018/05/29
+ * @content 前端安全
+ * 
+ * 1.浏览器安全
+ *    同源策略：限制了来自不同源的document或者脚本，对当前document读取或者设置某些属性。
+ *            影响“源”的因素有：host -- 协议，域名，端口
+ *            对于浏览器来说，DOM, Cookie, XMLHttpRequest会受到同源策略的限制
+ * 2.跨站脚本攻击
+ * 3.跨站请求伪造
+ * 4.点击劫持
+ */
