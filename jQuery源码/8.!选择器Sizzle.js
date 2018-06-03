6d40dcc0cfeac2efa5c730f3e2fdd4dbc171440f
shihangbo <shihangbo@126.com>
2018-06-03 22:49:33 +0800

    A

--- jQuery源码/8.!选择器Sizzle.js
+++ jQuery源码/8.!选择器Sizzle.js
@@ -10,10 +10,10 @@ var content = 'Sizzle 是一款纯js实现的css选择器引擎。在Sizzle内�
 var TITLE = '学习重点: 分析Sizzle的设计思路，工作原理，源码实现(特别是浏览器不支持方法querySelectorAll的情况)，jQuery对Sizzle的整合和拓展'
 
 var TITLE_1 = '分析Sizzle的设计思路'
-var CONTENT_1 = 'Sizzle 是一款从有想做查找的选择器引擎(如 div.red>p 为什么要从右向左查找？)，提供的三个步骤的核心接口：'
-                + '正则 chunker 负责从选择器表达式中提取快表达式和块关系符；'
-                + '方法 Sizzle.find() 负责查找快表达式匹配的元素集合，方法 Sizzle.filter() 负责用块关系符过滤元素集合；'
-                + '对象 Sizzle.selector.relative 中的块关系过滤函数根据块间关系符过滤元素集合；'
+var CONTENT_1 = 'Sizzle 是一款从从右向左查找的选择器引擎(如 div.red>p 为什么要从右向左查找？)，提供的三个步骤的核心接口：'
+                + '正则 chunker 负责从选择器表达式中提取块表达式和块关系符'
+                + '方法 Sizzle.find() 负责查找快表达式匹配的元素集合，方法 Sizzle.filter() 负责用块关系符过滤元素集合'
+                + '对象 Sizzle.selector.relative 中的块关系过滤函数根据块间关系符过滤元素集合'
 
 
 var TITLE_3 = 'jQuery对Sizzle的整合和拓展'
